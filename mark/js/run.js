91030119208c53b880a39f2ee1f0bbcaefa04045
Manu <90511900+Manumanoharan@users.noreply.github.com>
2022-07-19 13:37:39 +0530

    Add files via upload

---
+++ mark/js/run.js
@@ -0,0 +1,14 @@
+var marks=[95,96,97,99,100];
+var total=0;
+var length=marks.length;
+for (i=0;i<length;i++)
+{
+   
+  var total=total+marks[i];
+  
+  console.log(total); 
+
+}
+var avg=total/length;
+   console.log(avg);
+
